16e4864287e3440c16563264c5c6589a215841ca
Lindo-code <sdrowvieli1@gmail.com>
2022-04-22 21:02:34 +0200

    updated html, css and js

--- Script/script.js
+++ Script/script.js
@@ -1,14 +1,24 @@
-mybutton = document.getElementById("Navbar3");
+mybutton = document.getElementById("nav3");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
   scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    Navbar3.style.display = "block";
-    Nav2.style.display = "none";
+    nav3.style.display = "block";
+    nav2.style.display = "none";
   } else {
-    Navbar3.style.display = "none";
-    Nav2.style.display = "block";
+    nav3.style.display = "none";
+    nav2.style.display = "block";
   }
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
